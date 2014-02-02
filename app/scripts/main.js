0e85dbf5a4422467ee06545776ae1ae932b630f4
Bernardo <bernardo@insignia4u.com>
2014-02-02 13:39:02 -0300

    Finishes Resume Section. Adds easytabs. Adds icons.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$('#container').easytabs({
+    animate     : true,
+    updateHash    : false,
+    transitionIn    :'slideDown',
+    transitionOut   :'slideUp',
+    animationSpeed  :600,
+    tabs        :'.tmenu',
+    tabActiveClass  :'active',
+});
+
